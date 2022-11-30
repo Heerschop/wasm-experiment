237398e8239ccd7b7df2192bae8e269d107b5ad5
Bas Heerschop <heerschop@gmail.com>
2022-11-30 08:07:14 +0100

    Example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log('Hi');
+async function onClickAssemblyScript() {
+  console.log('onClickAssemblyScript');
+
+  const module = await loadWasmModule('examples/assembly-script.wasm')
+
+  console.log(module);
+}
+
+async function onClickEmscripten() {
+  console.log('onClickEmscripten');
+
+  const module = await loadWasmModule('examples/emscripten-c.wasm')
+
+  console.log(module);
+}
+
+async function onClickDotnet() {
+  console.log('onClickDotnet');
+
+  const module = await loadWasmModule('examples/dotnet.wasm')
+
+  console.log(module);
+}
+
+async function loadWasmModule(url: string) {
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+
+  return WebAssembly.compile(buffer);
+}
