edf45c4a8eef595fb466dbf1fea7d298c6c76447
Bas Heerschop <heerschop@gmail.com>
2023-02-15 07:54:57 +0100

    .

--- assembly-script/examples/example-tsc/main.ts
+++ assembly-script/examples/example-tsc/main.ts
@@ -1,10 +1,11 @@
-import { add } from './build/wasm.js';
+import { add, text } from './build/wasm.js';
 
 async function main(): Promise<void> {
   console.log('Hello Kerbin!');
 
   console.log();
   console.log('add:', add(10, 20));
+  console.log('text:', text());
 }
 
 main();
