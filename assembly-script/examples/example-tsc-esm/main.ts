883e9d4f0003463ab443079a5a90fc50c6f01151
Bas Heerschop <heerschop@gmail.com>
2023-02-18 12:01:35 +0100

    .

--- assembly-script/examples/example-tsc-esm/main.ts
+++ assembly-script/examples/example-tsc-esm/main.ts
@@ -1,5 +1,6 @@
-import { add, set, text } from './build/wasm.js';
-import { WASM } from './wasm-loader.js';
+import * as exports from './build/wasm.js';
+
+const { add, set, text, memory } = exports;
 
 async function main(): Promise<void> {
   console.log('Hello Kerbin!');
@@ -7,43 +8,32 @@ async function main(): Promise<void> {
   console.log();
   console.log('add:', add(10, 20));
 
+  set(0);
   console.log('text:', text());
   set(1);
   console.log('text:', text());
 
-  const imports: WebAssembly.Imports = {
-    env: {
-      abort(message: any, fileName: any, lineNumber: any, columnNumber: any) {},
-    },
-  };
+  console.log();
+  console.log();
 
-  const module = await WASM.compile('build/wasm.wasm');
+  {
+    const instance = exports.instance;
+    const members = {
+      add: instance.exports.add as typeof add,
+      set: instance.exports.set as typeof set,
+      text: instance.exports.text as () => number,
+      memory: instance.exports.memory as WebAssembly.Memory,
+    };
 
-  console.log({
-    exports: WebAssembly.Module.exports(module),
-    imports: WebAssembly.Module.imports(module),
-  });
+    const pointerText = members.text();
+    const textDecoder = new TextDecoder('utf8');
+    const bytes = new Uint8Array(members.memory.buffer, pointerText, 100);
 
-  const instance = await WASM.instantiate(module, imports);
-  const exports = {
-    add: instance.exports.add as typeof add,
-    set: instance.exports.set as typeof set,
-    text: instance.exports.text as typeof text,
-    memory: instance.exports.memory as WebAssembly.Memory,
-  };
-
-  exports.set(1);
-
-  const pointerText = exports.text() as any as number;
-  const textDecoder = new TextDecoder('utf8');
-  const bytes = new Uint8Array(exports.memory.buffer, pointerText, 100);
-
-  console.log('memory:', exports.memory);
-  console.log('pointerText:', pointerText);
-  console.log('bytes:', bytes);
-  console.log('text:', textDecoder.decode(bytes));
-
-  //new URL("wasm.wasm", import.meta.url)
+    console.log('memory:', memory === members.memory);
+    console.log('pointerText:', pointerText);
+    console.log('bytes:', bytes);
+    console.log('text:', textDecoder.decode(bytes));
+  }
 }
 
 main();
