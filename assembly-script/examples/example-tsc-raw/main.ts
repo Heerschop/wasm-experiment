2bae90fc34622b4cb850cf32fa99ccdff9f14383
Bas Heerschop <heerschop@gmail.com>
2023-02-18 07:05:43 +0100

    .

--- assembly-script/examples/example-tsc-raw/main.ts
+++ assembly-script/examples/example-tsc-raw/main.ts
@@ -1,4 +1,50 @@
+import { PathLike } from 'node:fs';
+import fs from 'node:fs/promises';
+import { instantiate, __AdaptedExports } from './build/wasm.js';
+
 async function main(): Promise<void> {
-  console.log('Hi');
+  const buffer = await fs.readFile('build/wasm.wasm');
+  const module = await WebAssembly.compile(buffer);
+
+  console.log({
+    exports: WebAssembly.Module.exports(module),
+    imports: WebAssembly.Module.imports(module),
+  });
+
+  const imports = {
+    env: {
+      abort(message: any, fileName: any, lineNumber: any, columnNumber: any) {},
+    },
+  };
+
+  const { exports, instance } = await instantiate(module, imports);
+  {
+    exports.set(0);
+    console.log('text:', exports.text());
+    exports.set(1);
+    console.log('text:', exports.text());
+  }
+
+  console.log();
+  console.log();
+
+  {
+    //const instance = await WebAssembly.instantiate(module, imports);
+    const exports = {
+      add: instance.exports.add as typeof __AdaptedExports.add,
+      set: instance.exports.set as typeof __AdaptedExports.set,
+      text: instance.exports.text as () => number,
+      memory: instance.exports.memory as WebAssembly.Memory,
+    };
+    //exports.set(1);
+
+    const pointerText = exports.text() as any as number;
+    const textDecoder = new TextDecoder('utf8');
+    const bytes = new Uint8Array(exports.memory.buffer, pointerText, 100);
+
+    console.log('pointerText:', pointerText);
+    console.log('bytes:', bytes);
+    console.log('text:', textDecoder.decode(bytes));
+  }
 }
 main();
