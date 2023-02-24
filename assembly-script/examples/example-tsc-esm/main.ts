9bfb15b95d3b1d412617f0c97c4b193994981b60
Bas Heerschop <heerschop@gmail.com>
2023-02-24 09:55:00 +0100

    .

--- assembly-script/examples/example-tsc-esm/main.ts
+++ assembly-script/examples/example-tsc-esm/main.ts
@@ -1,108 +1,7 @@
 import * as exports from './build/wasm.js';
+import { hexDump } from './debugging.js';
 
 const { add, set, text, memory } = exports;
-
-const colors = {
-  red: '\x1b[31m',
-  green: '\x1b[32m',
-  yellow: '\x1b[33m',
-  orange: '\x1b[38;2;255;165;0m',
-  blue: '\x1b[94m',
-  cyan: '\x1b[36m',
-  bold: '\x1b[1m',
-  dim: '\x1b[2m',
-  normal: '\x1b[0m',
-};
-
-function dumpLine(bytes: ArrayLike<number>, start: number, end: number, textDecoder: TextDecoder, columnCount: number, columnWidth: number): number {
-  const length = columnWidth * columnCount;
-  let data = '';
-  let text = '';
-
-  const address = Math.floor(start / length) * length;
-
-  for (let index = 0; index < length; index++) {
-    const offset = address + index;
-    const byte = offset >= start && offset < end ? bytes[offset] : undefined;
-
-    if (index % columnWidth === 0) {
-      data += '   ';
-      text += '  ';
-    }
-    if (byte !== undefined) {
-      const value = textDecoder.decode(Uint8Array.of(byte), { stream: true });
-      text += value.replace(/[\u0000-\u001F\u007F-\u009F\�]/g, '.')[0] ?? '.';
-
-      data += byte.toString(16).padStart(2, '0') + ' ';
-    } else {
-      text += ' ';
-      data += '   ';
-    }
-  }
-
-  console.log(
-    `${colors.bold + address.toString(16).padStart(8, '0') + colors.normal} ` +
-      `${colors.dim + data + colors.normal}   ` +
-      `${colors.bold + '|' + text + '  |' + colors.normal}`,
-  );
-
-  return address + length;
-}
-
-function hexDump(bytes: ArrayLike<number>): void;
-function hexDump(bytes: ArrayLike<number>, decoder: string): void;
-function hexDump(bytes: ArrayLike<number>, offset: number): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, length?: number): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, decoder: string): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, length: number, decoder: string): void;
-function hexDump(bytes: ArrayLike<number>, decoder: string, columnCount: number): void;
-function hexDump(bytes: ArrayLike<number>, decoder: string, columnCount: number, columnWidth: number): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, decoder: string, columnCount: number): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, decoder: string, columnCount: number, columnWidth: number): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, length: number, decoder: string, columnCount: number): void;
-function hexDump(bytes: ArrayLike<number>, offset: number, length: number, decoder: string, columnCount: number, columnWidth: number): void;
-function hexDump(
-  bytes: ArrayLike<number>,
-  arg1: string | number = 'ascii',
-  arg2: string | number = 'ascii',
-  arg3: string | number = 'ascii',
-  arg4?: number,
-  arg5?: number,
-): void {
-  let offset = 0;
-  let length = bytes.length;
-  let decoder = 'ascii';
-  let columnWidth = 8;
-  let columnCount = 4;
-
-  if (typeof arg1 === 'string') {
-    decoder = arg1;
-    if (typeof arg2 === 'number') columnCount = arg2;
-    if (typeof arg3 === 'number') columnWidth = arg3;
-  } else if (typeof arg2 === 'string') {
-    decoder = arg2;
-    if (typeof arg1 === 'number') offset = arg1;
-    if (typeof arg3 === 'number') columnCount = arg3;
-    if (typeof arg4 === 'number') columnWidth = arg4;
-  } else if (typeof arg3 === 'string') {
-    decoder = arg3;
-    if (typeof arg1 === 'number') offset = arg1;
-    if (typeof arg2 === 'number') length = arg2;
-    if (typeof arg4 === 'number') columnCount = arg4;
-    if (typeof arg5 === 'number') columnWidth = arg5;
-  } else {
-    if (typeof arg1 === 'number') offset = arg1;
-    if (typeof arg2 === 'number') length = arg2;
-  }
-
-  // //function hexDump(bytes: ArrayLike<number>, offset: number = 0, length?: number, decoder: string = 'ascii', columnWidth = 8, columnCount = 4): void {
-  const end = length === undefined ? bytes.length : offset + length;
-  const textDecoder = new TextDecoder(decoder, { ignoreBOM: false, fatal: false });
-
-  while (offset < end) {
-    offset = dumpLine(bytes, offset, end, textDecoder, columnCount, columnWidth);
-  }
-}
 
 async function main(): Promise<void> {
   console.log('Hello Kerbin!');
@@ -135,6 +34,8 @@ async function main(): Promise<void> {
     console.log('pointerText:', pointerText);
     console.log('bytes:', bytes);
     console.log('text:', textDecoder.decode(bytes));
+
+    //hexDump(new Uint8Array(members.memory.buffer), 0, 1000, 'utf8');
   }
 
   //hexDump(new Uint8Array(memory.buffer, 0, 1024));
@@ -169,10 +70,7 @@ const bytes = [
   0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff
 ];
 
-const textDecoder = new TextDecoder('utf8');
-const textEncoder = new TextEncoder();
-
-// hexDump(textEncoder.encode('Hallo "€"'));
+hexDump(bytes, 'ascii');
 // hexDump(textEncoder.encode('Hallo "€"'), undefined, undefined, new TextDecoder('utf8'));
 // hexDump(textEncoder.encode('Hallo "€"'), undefined, undefined, new TextDecoder('ascii'));
 
@@ -187,7 +85,7 @@ const textEncoder = new TextEncoder();
 // hexDump(array, 'utf-8');
 // hexDump(array, 0, 10);
 
-hexDump(bytes, 0x03,0x10 ,'ascii', 2, 4);
-console.log();
-console.log();
-hexDump(bytes, 'utf8');
+//hexDump(bytes, 0x03, 0x10, 'ascii', 2, 4);
+//console.log();
+//console.log();
+//hexDump(bytes, 'utf8');
