aa8d4a05f235b35557648e6becd56bc92091110b
maria <m.djordjanova@gmail.com>
2023-02-25 12:34:27 +0100

    wasm json experiments

--- assembly-script/examples/wasm-json/main.ts
+++ assembly-script/examples/wasm-json/main.ts
@@ -1,1 +1,14 @@
+import { add, text } from './build/wasm.js';
+import { instance } from './build/wasm.js';
+
 console.log('hi');
+
+const result = add(2, 3);
+console.log(result);
+
+const textResult = text();
+console.log(textResult);
+
+// const aa = (instance.exports as any).text();
+// console.log(aa);
+console.log(instance);
